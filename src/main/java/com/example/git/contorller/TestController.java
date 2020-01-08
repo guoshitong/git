3964534d37ca3b3600ebe9d3e38fc1a3d15e74b4
yintianchou <110@666.com>
2020-01-08 10:10:31 +0800

    âredis

--- src/main/java/com/example/git/contorller/TestController.java
+++ src/main/java/com/example/git/contorller/TestController.java
@@ -1,19 +1,28 @@
 package com.example.git.contorller;
 
+import com.example.git.util.RedisString;
+
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author yintianchou
  * @description TODO
  * @date 2019/12/26上午1:13
  */
-@RestController("/test")
+@RestController
 public class TestController {
+
+    @Resource
+    private RedisString redisString;
 
     @GetMapping("hello")
     public String sayHello(){
-        return "hello";
+        redisString.set();
+        return "牛逼";
     }
 }
 
